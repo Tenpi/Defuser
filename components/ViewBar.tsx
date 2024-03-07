6c526ed83e2648d629cba2018c1a982cabd8dcba
Tenpi <pie4chan@gmail.com>
2024-03-07 04:17:18 -0500

    updates

--- components/ViewBar.tsx
+++ components/ViewBar.tsx
@@ -3,10 +3,47 @@ import {useHistory} from "react-router-dom"
 import {HashLink as Link} from "react-router-hash-link"
 import favicon from "../assets/icons/favicon.png"
 import {EnableDragContext, MobileContext, SiteHueContext, SiteSaturationContext, SiteLightnessContext, 
-ViewImagesContext, UpdateImagesContext, GeneratorContext} from "../Context"
+ViewImagesContext, UpdateImagesContext, GeneratorContext, ImageBrightnessContext, ImageContrastContext} from "../Context"
 import functions from "../structures/Functions"
+import downloadIcon from "../assets/icons/download.png"
 import "./styles/viewbar.less"
 import axios from "axios"
+import JSZip from "jszip"
+import path from "path"
+
+interface ViewImageProps {
+    img: string
+    update: (img: string) => void
+}
+
+const ViewImage: React.FunctionComponent<ViewImageProps> = (props) => {
+    const {imageBrightness, setImageBrightness} = useContext(ImageBrightnessContext)
+    const {imageContrast, setImageContrast} = useContext(ImageContrastContext)
+    const [hover, setHover] = useState(false)
+
+    const download = async () => {
+        const images = await axios.post("/similar-images", {image: props.img}).then((r) => r.data)
+        const zip = new JSZip()
+        for (let i = 0; i < images.length; i++) {
+            const data = await fetch(images[i]).then((r) => r.arrayBuffer())
+            zip.file(`${path.basename(images[i], path.extname(images[i]))}${path.extname(images[i])}`, data, {binary: true})
+        }
+        const filename = "images.zip"
+        const blob = await zip.generateAsync({type: "blob"})
+        const url = window.URL.createObjectURL(blob)
+        functions.download(filename, url)
+        window.URL.revokeObjectURL(url)
+    }
+
+    return (
+        <div className="view-image-container" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+            <div className={`view-image-button-container ${hover ? "show-view-image-buttons" : ""}`}>
+                <img className="view-image-button" src={downloadIcon} onClick={download} draggable={false}/>
+            </div>
+            <img className="view-image" src={props.img} onClick={() => props.update(props.img)} draggable={false} style={{filter: `brightness(${imageBrightness + 100}%) contrast(${imageContrast + 100}%)`}}/>
+        </div>
+    )
+}
 
 const ViewBar: React.FunctionComponent = (props) => {
     const [ignored, forceUpdate] = useReducer(x => x + 1, 0)
@@ -63,7 +100,7 @@ const ViewBar: React.FunctionComponent = (props) => {
         let saved = localStorage.getItem(getSaveKey()) || "[]" as any
         saved = JSON.parse(saved)
         for (let i = 0; i < saved.length; i++) {
-            jsx.push(<img onClick={() => updateViewImages(saved[i])} className="view-image" src={saved[i]}/>)
+            jsx.push(<ViewImage img={saved[i]} update={updateViewImages}/>)
         }
         return jsx
     }
--- pages/SavedPrompts.tsx
+++ pages/SavedPrompts.tsx
@@ -8,6 +8,8 @@ import functions from "../structures/Functions"
 import CheckpointBar from "../components/CheckpointBar"
 import "./styles/generate.less"
 import axios from "axios"
+import imagesMeta from "images-meta"
+import path from "path"
 
 let timeout = null as any
 
@@ -43,9 +45,9 @@ const SavedPrompts: React.FunctionComponent = (props) => {
         localStorage.setItem("savedPrompts-holara-ai", JSON.stringify(savedPromptsHolaraAI))
     }, [savedPrompts, savedPromptsNovelAI, savedPromptsHolaraAI])
 
-    const changeSavedPrompts = (event: any) => {
+    const changeSavedPrompts = (value: string) => {
         if (timeout) clearTimeout(timeout)
-        const prompts = event.target.value.split("\n")
+        const prompts = value.split("\n")
         if (generator === "novel ai") {
             setSavedPromptsNovelAI(prompts)
         } else if (generator === "holara ai") {
@@ -64,13 +66,64 @@ const SavedPrompts: React.FunctionComponent = (props) => {
         return savedPrompts.join("\n")
     }
 
+    const getSaveKey = () => {
+        if (generator === "novel ai") return "saved-novel-ai"
+        if (generator === "holara ai") return "saved-holara-ai"
+        return "saved"
+    }
+
+    const getPrompt = async (img: string) => {
+        let inMime = "image/jpeg"
+        if (path.extname(img) === ".png") inMime = "image/png"
+        if (path.extname(img) === ".webp") inMime = "image/webp"
+        const arrayBuffer = await fetch((img)).then((r) => r.arrayBuffer())
+        if (inMime === "image/png") {
+            const meta = imagesMeta.readMeta(Buffer.from(arrayBuffer), inMime)
+            const prompt = meta.find((m: any) => m.name?.toLowerCase() === "prompt")?.value || ""
+            return prompt
+        } else if (inMime === "image/jpeg") {
+            let str = ""
+            const meta = imagesMeta.readMeta(Buffer.from(arrayBuffer), inMime)
+            for (let i = 0; i < meta.length; i++) {
+                if (meta[i].name?.toLowerCase() === "usercomment") {
+                    str +=  meta[i].value.replaceAll("UNICODE", "").replaceAll(/\u0000/g, "")
+                }
+            }
+            const {prompt} = functions.extractMetaValues(str)
+            return prompt
+        } else {
+            const form = new FormData()
+            const blob = new Blob([new Uint8Array(arrayBuffer)])
+            const file = new File([blob],img)
+            form.append("image", file)
+            const exif = await axios.post("get-exif", form).then((r) => r.data)
+            const str = exif.replaceAll("UNICODE", "").replaceAll(/\u0000/g, "")
+            const {prompt} = functions.extractMetaValues(str)
+            return prompt
+        }
+    }
+
+    const importSaved = async () => {
+        let saved = localStorage.getItem(getSaveKey()) || "[]" as any
+        saved = JSON.parse(saved)
+        let prompts = [] as any
+        for (let i = 0; i < saved.length; i++) {
+            const prompt = await getPrompt(saved[i])
+            prompts.push(prompt)
+        }
+        changeSavedPrompts(prompts.join("\n"))
+    }
+
     return (
         <div className="generate" onMouseEnter={() => setEnableDrag(false)}>
             <CheckpointBar/>
             <div className="settings-bar">
                 <div className="settings-bar-row">
                     <span className="settings-bar-text">Saved Prompts:</span>
-                    <textarea className="settings-bar-textarea" style={{minHeight: "75vh", width: "70vw"}} spellCheck={false} value={getSavedPrompts()} onChange={(event) => changeSavedPrompts(event)}></textarea>
+                    <textarea className="settings-bar-textarea" style={{minHeight: "75vh", width: "70vw"}} spellCheck={false} value={getSavedPrompts()} onChange={(event) => changeSavedPrompts(event.target.value)}></textarea>
+                </div>
+                <div className="settings-bar-row">
+                    <button className="settings-bar-button" onClick={() => importSaved()}>Import From Saved</button>
                 </div>
             </div>
         </div>
